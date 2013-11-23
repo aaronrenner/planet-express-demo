2c2d6bf4c199316dd5a635f22f7901509136f60b
Aaron Renner <aaron@animascodelabs.com>
2013-11-23 08:50:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetExpress::Application.config.secret_key_base = 'fd9da21256d3c487e1b13d5621866b1a2658729deeb6fba6ed4921e1414b3bd0865af83e12a4eed25ea853248d9910d1af01cece3b90db33c98dce853713fadc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlanetExpress::Application.config.session_store :cookie_store, key: '_planet_express_session'
